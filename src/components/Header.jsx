a5e40a253f5da70c167e8da65aca0a415aab0206
Serge Toure <26191503+sergetoure09@users.noreply.github.com>
2025-06-25 23:28:45 +0200

    adding meta tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { Nav } from "./Nav";
 import reactLogo from "../assets/react.svg";
+import { HStack } from "@chakra-ui/react";
 
 export const Header = () => {
   return (
     <>
       <header>
-        <img src={reactLogo} className="logo react" alt="React logo" />
-        <Nav />
+        <HStack>
+          <img src={reactLogo} className="logo react" alt="React logo" />
+          <Nav />
+        </HStack>
       </header>
     </>
   );
